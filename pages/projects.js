04ccb53f17e6a7da2e69a0f951b2654548298343
Joel Pantoja <joelpantojaortega@gmail.com>
2021-07-03 13:17:53 -0700

    added color to projects section

--- pages/projects.js
+++ pages/projects.js
@@ -4,30 +4,37 @@ const projects = [
 		image: "/flow.PNG",
 		video: "/flow-scommerce.mp4",
 		github: "https://github.com/pantojao/Flow-Ecommerce",
+		class: " book-shadow",
 	},
 	{
 		name: "Nasa Image Generator",
 		image: "/nasa-app-pic.PNG",
 		video: "/nasa.mp4",
 		github: "https://github.com/pantojao/nasa-app",
+		class: " nasa-shadow",
 	},
 	{
 		name: "Pac-Man Search",
 		image: "/pac-man-image.PNG",
 		video: "/pacman.mp4",
 		github: "https://github.com/pantojao/pac-man-search",
+		class: " pac-shadow",
 	},
 	{
 		name: "IPA Finder",
 		image: "/ipa-app-pic.PNG",
 		video: "/ipa.mp4",
 		github: "https://github.com/pantojao/ipa-app",
+		class: " beer-shadow",
 	},
 ];
 
 const Projects = () => {
 	return (
-		<section className=" mt-10 space-y-20 flex flex-col justify-center items-center">
+		<section className="mt-10 space-y-20 flex flex-col justify-center items-center">
+			<h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-blue-800 p-2">
+				Featured Projects
+			</h1>
 			{projects.map((project) => (
 				<div className="flex flex-col justify-center items-center space-y-10 px-2">
 					<div className="flex justify-between items-center w-full ">
@@ -37,7 +44,11 @@ const Projects = () => {
 						<img src="/github.svg" className="w-10 h-10" />
 					</div>
 
-					<div className="relative shadow-lg rounded-md">
+					<div
+						className={
+							"relative shadow-lg rounded-md" +
+							project.class
+						}>
 						<video
 							preload="auto"
 							poster={project.image}
