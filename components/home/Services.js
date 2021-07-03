101be1a58252b7a852004e635ada989df623bd6b
Joel Pantoja <joelpantojaortega@gmail.com>
2021-07-03 11:24:57 -0700

    fixed the services layout

--- components/home/Services.js
+++ components/home/Services.js
@@ -18,31 +18,28 @@ const Services = () => {
 	const isVisible = useOnScreen(badge);
 
 	return (
-		<div
-			ref={badge}
-			className="justify-items-start items-center flex flex-col justify-center container md:grid md:grid-cols-2 md:grid-row-3 md:px-20 h-auto space-y-4  blue-outline">
-			<h2 className="font-bold col-span-2 row-span-1 grid-row-start-1 md:col-span-1 text-5xl blue-outline">
+		<div className="justify-items-start items-end container md:grid md:grid-cols-2 md:grid-rows-2 lg:px-20 space-y-8">
+			<h2 className="font-bold col-span-2 row-span-1 grid-row-start-1 md:col-span-1 text-5xl">
 				Experience In
 			</h2>
 
-			<Transition
-				show={isVisible}
-				enter="transition-opacity duration-1000"
-				enterFrom="opacity-0"
-				enterTo="opacity-100"
-				leave="transition-opacity duration-150"
-				leaveFrom="opacity-100"
-				leaveTo="opacity-0">
-				<div className="w-5/6 mx-auto my-auto md:col-span-1 md:row-span-2 flex flex-wrap space-evenly red-outline">
-					{services.map((service) => (
-						<span className="inline-flex items-center px-4 py-3 text-sm font-medium bg-gray-100 text-gray-800 rounded-full m-1">
-							{service}
-						</span>
-					))}
-				</div>
-			</Transition>
+			<div ref={badge} className="row-span-2">
+				<Transition
+					show={isVisible}
+					enter="transition-opacity duration-1000"
+					enterFrom="opacity-0"
+					enterTo="opacity-100">
+					<div className="w-5/6 md:w-full mx-auto flex flex-wrap space-evenly">
+						{services.map((service) => (
+							<span className="inline-flex items-center px-4 py-3 text-sm font-medium bg-gray-100 rounded-full m-1">
+								{service}
+							</span>
+						))}
+					</div>
+				</Transition>
+			</div>
 
-			<button className="grid-row-start-3 col-span-2 md:col-span-1 md:row-span-1 md:grid-row-start-2 inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+			<button className="self-start md:grid-row-start-2 md:col-span-1 inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 				<a href="/contact">Contact Me</a>
 			</button>
 		</div>
