3d4854db1d720defee65eb5822d9b4aa60ef91bb
Joel Pantoja <joelpantojaortega@gmail.com>
2021-07-07 07:40:54 -0700

    project button now redirects

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => {
 					<div className="mt-10 sm:flex sm:justify-center lg:justify-start">
 						<div className="rounded-md shadow">
 							<a
-								href="#"
+								href="/projects"
 								className="w-full flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-8">
 								View Projects
 							</a>
