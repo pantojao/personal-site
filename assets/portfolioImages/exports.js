82de896031eca81943347b5d25a9e44122b433be
Joel Pantoja <joelpantojaortega@gmail.com>
2021-07-11 09:32:37 -0700

    preview section is now responsive

--- assets/portfolioImages/exports.js
+++
@@ -1,6 +0,0 @@
-import ent from "./ent.JPG";
-import accounting from "./accounting.PNG";
-import glacier from "./glacier.JPG";
-import iron from "./iron.JPG";
-
-export { ent, accounting, glacier, iron };
--- components/home/Previews.js
+++ components/home/Previews.js
@@ -1,10 +1,17 @@
 const items = [
-	// {
-	// 	name: "Insure Landing Page",
-	// 	url: "/previews/insure-preview.jpg",
-	// 	description: "This is a landing page for a ",
-	// 	tech: ["Next.js", "Tailwind CSS"],
-	// },
+	{
+		name: "Utlimate Iron Crafts",
+		url: "/previews/iron.jpg",
+		description:
+			"This was a landing page built for accounting firm. ",
+		tech: ["Next.js", "Tailwind CSS"],
+	},
+	{
+		name: "Insure Landing Page",
+		url: "/previews/insure-preview.jpg",
+		description: "This is a landing page for a ",
+		tech: ["Next.js", "Tailwind CSS"],
+	},
 	{
 		name: "Inland Taxes",
 		url: "/previews/inland-preview.png",
@@ -33,8 +40,9 @@ const items = [
 ];
 
 const Previews = () => {
+	// sm:bg-blue-300 md:bg-yellow-300 xl:bg-red-300sm:bg-blue-300 md:bg-yellow-300 xl:bg-red-300
 	return (
-		<section class="space-y-20 w-full container">
+		<section class="space-y-20 container ">
 			<h1 class="text-black font-bold text-5xl">
 				Featured Sites
 			</h1>
@@ -43,22 +51,15 @@ const Previews = () => {
 				return (
 					<article
 						class={
-							"overflow-hidden relative container flex " +
-							(odd ? " flex-row-reverse " : "flex-row")
+							"overflow-hidden relative container items-center justify-center flex py-4" +
+							(odd
+								? " lg:flex-row-reverse lg:justify-between"
+								: " lg:justify-between lg:flex-row")
 						}>
-						<div
-							className="relative"
-							style={{
-								minWidth: "700px",
-								minHeight: "500px",
-							}}>
+						<div className="relative sm-img md:md-img lg:lg-img">
 							<img
 								src={item.url}
-								className="absolute object-cover"
-								style={{
-									minWidth: "700px",
-									minHeight: "500px",
-								}}
+								className="absolute object-contain"
 							/>
 							<h1>{item.name}</h1>
 							<p>{item.description}</p>
@@ -66,23 +67,25 @@ const Previews = () => {
 
 						<div
 							class={
-								"hidden md:block text-white bg-white absolute w-1/2 h-40 bottom-24 text-black shadow-2xl p-4 " +
+								"hidden lg:block text-white bg-white relative w-1/2 h-40 top-4 shadow-2xl " +
 								(odd ? "left-20" : "right-20")
 							}>
 							<h2
 								class={
-									"text-3xl -top-24 text-black font-bold relative w-full " +
-									(odd ? "text-left" : "text-right")
+									"text-2xl xl:text-3xl -top-24 text-black font-bold absolute w-1/2 " +
+									(odd
+										? "text-left"
+										: " right-0 text-right")
 								}>
 								{item.name}
 							</h2>
-							<p class="text-black text-lg font-semibold">
+
+							{/* <p class="text-black text-lg font-semibold">
 								{item.description}
 							</p>
 							<p class="text-black tracking-widest bottom-0 left">
 								{item.tech.join(", ")}
 							</p>
-
 							<div
 								class={
 									"flex space-x-6 -bottom-14 absolute " +
@@ -94,12 +97,12 @@ const Previews = () => {
 									class="w-10"
 								/>
 								<img src="/export.svg" alt="" class="w-8" />
-							</div>
+							</div> */}
 						</div>
 					</article>
 				);
 			})}
-			{/* </div> */}
+
 			<button className="inline-flex items-center px-8 py-3 border border-transparent text-base font-medium rounded shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 				<a href="/projects">See More</a>
 			</button>
